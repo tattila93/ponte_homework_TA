02f23642ea6f4609b1fa3db7affcd45d0e85f119
tattila93 <107313118+tattila93@users.noreply.github.com>
2023-06-16 11:21:47 +0200

    Add files via upload

---
+++ documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"hu.ponte.hr"},{"l":"hu.ponte.hr.config"},{"l":"hu.ponte.hr.controller"},{"l":"hu.ponte.hr.controller.upload"},{"l":"hu.ponte.hr.domain"},{"l":"hu.ponte.hr.exceptions"},{"l":"hu.ponte.hr.repository"},{"l":"hu.ponte.hr.services"}];updateSearchResults();
